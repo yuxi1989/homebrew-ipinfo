69916651334dd501476c7b9038febca8572f3b40
zhuxiaoyu <zxy@ip669.com>
2026-02-11 21:52:00 +0800

    Update source repo URL from ipinfo to ipcn

--- Formula/ipcn.rb
+++ Formula/ipcn.rb
@@ -1,7 +1,7 @@
 class Ipcn < Formula
   desc "IP/Domain geolocation & ASN lookup CLI"
-  homepage "https://github.com/yuxi1989/ipinfo"
-  url "https://github.com/yuxi1989/ipinfo/archive/refs/tags/v0.2.0.tar.gz"
+  homepage "https://github.com/yuxi1989/ipcn"
+  url "https://github.com/yuxi1989/ipcn/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "afaa15cb1dffcd191a40c5045b4ff7add758a04255da8992098bcac644222fd1"
   license "MIT"
 
